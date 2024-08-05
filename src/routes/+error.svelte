5a95b9e3bdfaff3e2c205a798071499cdf7d5561
Melody Madeline Lyons <lily@nowaffles.com>
2024-08-04 20:25:10 -0700

    Center 404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,29 @@
   import { page } from "$app/stores";
 </script>
 
+<svelte:head>
+  <style>
+    html,
+    body {
+      height: 100%;
+    }
+
+    html {
+      display: table;
+      margin: auto;
+    }
+
+    body {
+      display: table-cell;
+      vertical-align: middle;
+    }
+
+    h1 {
+      text-align: center;
+    }
+  </style>
+</svelte:head>
+
 <h1>{$page.status} {$page.error?.message}</h1>
 <a href="https://http.cat">
   <img
