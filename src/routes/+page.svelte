1daa8eb1f7339e1cf0473da3472da31e50182fcf
Melody Madeline Lyons <lily@nowaffles.com>
2024-08-04 17:26:58 -0700

    Add blinkies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,97 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mel's site</title>
+    <style>
+      .blinkie {
+        height: 31px;
+        margin: 0 5px;
+      }
+
+      body {
+        background-image: url("0124-stars.gif");
+      }
+
+      h {
+        color: white;
+      }
+
+      /* Move to bottom of page */
+      footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        /* Make it purple */
+        background-color: rgba(128, 0, 196, 0.2);
+        /* Add a border */
+        border-top: 4px double rgba(39, 0, 60, 1);
+      }
+    </style>
+  </head>
+  <body>
+    <h>Mel's site!</h><br />
+
+    <footer>
+      <a href="https://blinkies.cafe" target="_blank"
+        ><img
+          src="https://blinkies.cafe/b/display/blinkiesCafe-badge.gif"
+          alt="blinkies.cafe | make your own blinkies!"
+          class="blinkie"
+        /></a
+      >
+      <a href="https://callistoashley.dev" target="_blank">
+        <img
+          src="queer.png"
+          class="blinkie"
+          alt="The text 'you're telling me a queer coded this' displayed on top of a rainbow gradient"
+        />
+      </a>
+      <img
+        src="slugcat.png"
+        class="blinkie"
+        alt="A slugcat from rain world next to the text 'slug ca'"
+      />
+      <img
+        src="archlinux.gif"
+        class="blinkie"
+        alt="The ArchLinux logo next to the text 'arch linux' on a marble background"
+        title="I use Arch btw"
+      />
+      <img
+        src="0235-rainbowstar.gif"
+        class="blinkie"
+        alt="The text 'SILLY!!' displayed next to a rainbow dancing star"
+        title="SILLY!!!!!!!!!!!!!!!!"
+      />
+      <img
+        src="0272-disabledpride.gif"
+        class="blinkie"
+        alt="The text 'Disabled Pride' surrounded by two disability pride flags"
+        title="I might not fall under the disability pride label, but I think it's important to show support!"
+      />
+      <img
+        src="blinkiesCafe-0r.gif"
+        class="blinkie"
+        alt="The text 'this user is trains' displayed on top of the transgender pride flag"
+      />
+      <img
+        src="blinkiesCafe-44.gif"
+        class="blinkie"
+        alt="The text 'demiromantic pride' displayed on top of the demiromantic pride flag"
+        title="Aro/Ace & Aroace pride!"
+      />
+      <img
+        src="blinkiesCafe-OJ.gif"
+        class="blinkie"
+        alt="The text 'lesbian pride' displayed on top of the lesbian pride flag"
+      />
+    </footer>
+  </body>
+</html>
