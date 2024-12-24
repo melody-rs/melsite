ac2ab5d3fd39d9ed8fd4ec118d87fd9a9c7e56e3
melody-rs <melody@melody-is.gay>
2024-12-24 14:43:40 -0800

    add basic gadget layout (will refactor later)

--- src/routes/gadgets/+page.svelte
+++ src/routes/gadgets/+page.svelte
@@ -11,9 +11,66 @@
 <ContentWrapper>
   <Navbar />
   <div class="container">
-    <a href="/gadgets/guestbook">Guestbook</a>
+    <section class="gadget-section">
+      <span>
+        <h1>
+          <img class="logo-svg" alt="" src="/logos/svelte.svg" /><a
+            href="/gadgets/guestbook">Guestbook</a
+          >
+        </h1>
+        A little list of people who've visited this site!
+      </span>
+      <img class="gadget-img" src="/files/guestbook.png" alt="" />
+    </section>
   </div>
+  <div style="padding-bottom: 20px"></div>
 </ContentWrapper>
 
 <style>
+  .logo-svg {
+    height: 32px;
+    filter: invert(100%);
+    vertical-align: middle;
+    padding-right: 16px;
+  }
+
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 64px;
+  }
+  .gadget-section {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    padding: 1rem;
+  }
+
+  .gadget-img {
+    height: 320px;
+    /* Use transparent so the background image is visible */
+    border: 5px solid transparent;
+    padding: 2px;
+    border-radius: 20px;
+    background-image: linear-gradient(
+      to right,
+      rgb(190, 50, 255),
+      rgb(89, 0, 255)
+    );
+    background-origin: border-box;
+    background-clip: padding-box, border-box;
+  }
+
+  span {
+    font-family: "Iosevka SS05 Web", monospace;
+    color: white;
+    font-size: 18px;
+    font-weight: 300;
+    margin: 16px;
+  }
 </style>
